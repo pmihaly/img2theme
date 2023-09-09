330c751b85d1b5670b6b9688498b389c4e7d7cc6
pmihaly <mihaly@mihaly.codes>
2023-09-09 20:30:22 +0200

    refactor to imagewrapper

--- main.go
+++ main.go
@@ -19,9 +19,169 @@ type ColorfulColor struct {
 	colorful.Color
 }
 
+func (c *ColorfulColor) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var hex string
+	if err := unmarshal(&hex); err != nil {
+		return err
+	}
+	color, err := colorful.Hex(hex)
+	if err != nil {
+		return err
+	}
+	*c = ColorfulColor{color}
+	return nil
+}
+
 type Settings struct {
 	Palette         []ColorfulColor `yaml:"palette"`
 	PaletteAffinity float64         `yaml:"palette-affinity"`
+}
+
+func LoadSettingsFromYaml(filePath string) (Settings, error) {
+	rawSettings, err := os.ReadFile(filePath)
+	if err != nil {
+		return Settings{}, err
+	}
+
+	settings := Settings{}
+	err = yaml.Unmarshal(rawSettings, &settings)
+	if err != nil {
+		return Settings{}, err
+	}
+
+	return settings, nil
+}
+
+type ImageMapper struct {
+	Settings           Settings
+	LoadedImage        image.Image
+	MappedImage        *image.RGBA
+	MappedColorByColor *sync.Map
+}
+
+func NewImageMapper(settings Settings, loadedImage image.Image) (*ImageMapper, error) {
+	mapper := &ImageMapper{
+		Settings:           settings,
+		MappedColorByColor: &sync.Map{},
+		LoadedImage:        loadedImage,
+		MappedImage:        image.NewRGBA(loadedImage.Bounds()),
+	}
+
+	return mapper, nil
+}
+
+func (im *ImageMapper) MapPixelToPalette(x, y int) {
+	currentPixelColor := im.LoadedImage.At(x, y)
+
+	if mappedColor, ok := im.MappedColorByColor.Load(currentPixelColor); ok {
+		im.MappedImage.Set(x, y, mappedColor.(color.Color))
+		return
+	}
+
+	targetLab, _ := colorful.MakeColor(currentPixelColor)
+	minDistance := math.Inf(1)
+	var mappedColor colorful.Color
+
+	for _, c := range im.Settings.Palette {
+		distance := targetLab.DistanceLab(c.Color)
+		if distance < minDistance {
+			minDistance = distance
+			mappedColor = c.Color
+		}
+	}
+
+	adjustedColor := colorful.Color{
+		R: targetLab.R + (mappedColor.R-targetLab.R)*im.Settings.PaletteAffinity,
+		G: targetLab.G + (mappedColor.G-targetLab.G)*im.Settings.PaletteAffinity,
+		B: targetLab.B + (mappedColor.B-targetLab.B)*im.Settings.PaletteAffinity,
+	}
+
+	im.MappedImage.Set(x, y, adjustedColor)
+	im.MappedColorByColor.Store(currentPixelColor, adjustedColor)
+}
+
+func (im *ImageMapper) mapImageRows(rowCh chan int) {
+	for row := range rowCh {
+		for x := im.LoadedImage.Bounds().Min.X; x < im.LoadedImage.Bounds().Max.X; x++ {
+			im.MapPixelToPalette(x, row)
+		}
+	}
+}
+
+func (im *ImageMapper) SaveAsJpeg(outputJpegPath string) error {
+	outputJpeg, err := os.Create(outputJpegPath)
+	if err != nil {
+		return err
+	}
+	defer outputJpeg.Close()
+
+	jpeg.Encode(outputJpeg, im.MappedImage, nil)
+
+	return nil
+}
+
+func loadImage(filePath string) (image.Image, error) {
+	inputFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer inputFile.Close()
+
+	img, _, err := image.Decode(inputFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+func mainAction(c *cli.Context) error {
+	inputImagePath := c.String("input")
+	outputImagePath := c.String("output")
+	settingsFilePath := c.String("settings")
+
+	settings, err := LoadSettingsFromYaml(settingsFilePath)
+	if err != nil {
+		return err
+	}
+
+	loadedImage, err := loadImage(inputImagePath)
+	if err != nil {
+		return err
+	}
+
+	mapper, err := NewImageMapper(settings, loadedImage)
+	if err != nil {
+		return err
+	}
+
+	numCPU := runtime.NumCPU()
+	var wg sync.WaitGroup
+	rowCh := make(chan int, numCPU)
+
+	for i := 0; i < numCPU; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mapper.mapImageRows(rowCh)
+		}()
+	}
+
+	for y := mapper.LoadedImage.Bounds().Min.Y; y < mapper.LoadedImage.Bounds().Max.Y; y++ {
+		rowCh <- y
+	}
+	close(rowCh)
+	wg.Wait()
+
+	err = mapper.SaveAsJpeg(outputImagePath)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Image mapped and saved at: " + outputImagePath)
+
+	return nil
 }
 
 func main() {
@@ -53,152 +213,4 @@ func main() {
 		fmt.Println(err)
 		cli.Exit(err, 1)
 	}
-}
-
-func mainAction(c *cli.Context) error {
-	inputImagePath := c.String("input")
-	outputImagePath := c.String("output")
-	settingsFilePath := c.String("settings")
-
-	settings, err := loadSettings(settingsFilePath)
-	if err != nil {
-		return err
-	}
-
-	loadedImage, err := loadImage(inputImagePath)
-	if err != nil {
-		return err
-	}
-
-	mappedImage := image.NewRGBA(loadedImage.Bounds())
-
-	mappedColorByColor := sync.Map{}
-	numCPU := runtime.NumCPU()
-	var wg sync.WaitGroup
-	rowCh := make(chan int, numCPU)
-
-	for i := 0; i < numCPU; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			mapImageRows(rowCh, loadedImage, settings, mappedImage, &mappedColorByColor)
-		}()
-	}
-
-	for y := loadedImage.Bounds().Min.Y; y < loadedImage.Bounds().Max.Y; y++ {
-		rowCh <- y
-	}
-	close(rowCh)
-	wg.Wait()
-
-	err = saveMappedImage(outputImagePath, mappedImage)
-
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Image mapped and saved at: " + outputImagePath)
-
-	return nil
-}
-
-func (c *ColorfulColor) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var hex string
-	if err := unmarshal(&hex); err != nil {
-		return err
-	}
-	color, err := colorful.Hex(hex)
-	if err != nil {
-		return err
-	}
-	*c = ColorfulColor{color}
-	return nil
-}
-
-func loadSettings(settingsFilePath string) (Settings, error) {
-	rawSettings, err := os.ReadFile(settingsFilePath)
-	if err != nil {
-		return Settings{}, err
-	}
-
-	var settings Settings
-	err = yaml.Unmarshal(rawSettings, &settings)
-	if err != nil {
-		return Settings{}, err
-	}
-
-	return settings, nil
-}
-
-func loadImage(inputImagePath string) (image.Image, error) {
-	inputFile, err := os.Open(inputImagePath)
-	if err != nil {
-		return nil, err
-	}
-	defer inputFile.Close()
-
-	img, _, err := image.Decode(inputFile)
-	if err != nil {
-		return nil, err
-	}
-
-	return img, nil
-}
-
-func parsePalette(palette []string) []colorful.Color {
-	var parsedPalette []colorful.Color
-	for _, hex := range palette {
-		c, _ := colorful.Hex(hex)
-		parsedPalette = append(parsedPalette, c)
-	}
-	return parsedPalette
-}
-
-func mapImageRows(rowCh chan int, originalImage image.Image, settings Settings, mappedImage *image.RGBA, mappedColorByColor *sync.Map) {
-	for row := range rowCh {
-		for x := originalImage.Bounds().Min.X; x < originalImage.Bounds().Max.X; x++ {
-			pixelColor := originalImage.At(x, row)
-			mapAndSetPixelColor(pixelColor, originalImage, settings, x, row, mappedImage, mappedColorByColor)
-		}
-	}
-}
-
-func mapAndSetPixelColor(pixelColor color.Color, originalImage image.Image, settings Settings, x, y int, mappedImage *image.RGBA, mappedColorByColor *sync.Map) {
-	if mappedColor, ok := mappedColorByColor.Load(pixelColor); ok {
-		mappedImage.Set(x, y, mappedColor.(color.Color))
-		return
-	}
-
-	targetLab, _ := colorful.MakeColor(pixelColor)
-	minDistance := math.Inf(1)
-	var mappedColor colorful.Color
-
-	for _, c := range settings.Palette {
-		distance := targetLab.DistanceLab(c.Color)
-		if distance < minDistance {
-			minDistance = distance
-			mappedColor = c.Color
-		}
-	}
-
-	adjustedColor := colorful.Color{
-		R: targetLab.R + (mappedColor.R-targetLab.R)*settings.PaletteAffinity,
-		G: targetLab.G + (mappedColor.G-targetLab.G)*settings.PaletteAffinity,
-		B: targetLab.B + (mappedColor.B-targetLab.B)*settings.PaletteAffinity,
-	}
-
-	mappedImage.Set(x, y, adjustedColor)
-	mappedColorByColor.Store(pixelColor, adjustedColor)
-}
-
-func saveMappedImage(outputImagePath string, mappedImage image.Image) error {
-	outputImageFile, err := os.Create(outputImagePath)
-	if err != nil {
-		return err
-	}
-	defer outputImageFile.Close()
-
-	jpeg.Encode(outputImageFile, mappedImage, nil)
-
-	return nil
 }
